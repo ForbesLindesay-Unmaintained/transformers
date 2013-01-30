a7bc97c4a2a92cb17d8467430ba2ba6a9e61f07e
ForbesLindesay <forbes@lindesay.co.uk>
2013-01-30 17:41:02 +0000

    Release

--- build/build.js
+++ build/build.js
@@ -3,9 +3,9 @@ var transformers = require('transformers');
 var express = require('express');
 var loadEngine = require('load-engine');
 
-var all = require('all');
+var all = require('then-all');
 
-var engines = [{}];
+var engines = [];
 /*
 Object.keys(transformers)
   .forEach(function (name) {
@@ -14,18 +14,48 @@ Object.keys(transformers)
     }
   })
 */
+
+function forward(v) {
+  if (/home\.jade$/.test(v)) {
+    return v.replace(/home\.jade$/, 'index.html');
+  } else if (/\.jade$/.test(v)) {
+    return v.replace(/\.jade$/, '/index.html');
+  } else {
+    return null;
+  }
+}
+function backward(v) {
+  if (/src(\/|\\)\w+(\/|\\)index\.html$/.test(v)) {
+    return v.replace(/(\/|\\)index\.html$/, '.jade');
+  } else  if (/(\/|\\)index\.html$/.test(v)) {
+    return v.replace(/index\.html$/, 'home.jade');
+  } else {
+    return null;
+  }
+}
 all(engines)
   .then(function () {
-    var t = transform(__dirname)
+    var t = transform(require('path').join(__dirname, 'src'))
       .using(function (t) {
-        t.add('.jade', '.html', [['jade', {filter_mixin: filterMixin, pretty: false}], renderFilters])
+        t.add(forward, backward, [['jade', {filter_mixin: filterMixin, pretty: false}], renderFilters])
         t.add('.js', '.js', 'uglify-js');
+        t.add('style.less', 'style.css', ['less', 'uglify-css']);
       })
-      .grep(function (path) { return !(/node_modules/.test(path) || /build\.js/.test(path) || /includes\.jade/.test(path)); });
+      .grep(function (path) { return !(/node_modules/.test(path) || /build\.js/.test(path) || /head\.jade/.test(path) || /mixins\.jade/.test(path)); });
     var app = express();
+    app.use(express.logger('dev'));
+    app.use(function (req, res, next) {
+      if (!/\./.test(req.path)) {
+        if (!/\/$/g.test(req.path)) {
+          return res.redirect(req.path + '/');
+        }
+        req.url = (req.url + '/index.html').replace(/\/\//g, '/');
+      }
+      next();
+    });
     app.use(t.dynamically());
     app.listen(3000);
-    //t.statically(require('path').join(__dirname, '..'));
+    t.statically(require('path').join(__dirname, '..'));
   })
   .then(null, function (err) {
     process.nextTick(function () {
@@ -70,6 +100,8 @@ function renderFilters(str, filename, cb) {
           } else {
             results[n] = res;
           }
+        }, function (err) {
+          results[n] = err.stack || err.message || JSON.stringify(err);
         }));
     }(match[1]));
   }
